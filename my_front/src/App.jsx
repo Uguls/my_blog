bcea3769113f63b37ffa4538a7a9607581a5eb71
Uguls <chobozaex@gmail.com>
2024-05-08 15:45:37 +0900

    vite migration

--- my_front/src/App.jsx
+++ my_front/src/App.jsx
@@ -1,42 +1,16 @@
-import './App.css';
-import Home from './components/Home';
-import React, {useState} from "react";
-import Header from "./components/Header";
-import {createGlobalStyle} from "styled-components";
-import {Route, Routes} from "react-router-dom";
-import Create from "./components/Create";
-import Auth from "./components/Auth";
-import Login from "./components/Login";
-import PostDetail from "./components/PostDetail";
-import {RecoilRoot} from "recoil";
+import "./App.css";
 
-const GlobalStyle = createGlobalStyle`
-* {padding: 0; margin: 0;}
-
-a {text-decoration: none; color: #333};
-
-ul,ol,li {list-style: none;}
-`
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 상태 생성
-
   return (
-    <RecoilRoot>
-      <>
-        <div className="App">
-          <GlobalStyle/>
-          <Header isLoggedIn={isLoggedIn}/>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/create" element={<Create/>}/>
-            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>}/>
-            <Route path="/auth" element={<Auth/>}/>
-            <Route path="/post/:postId" element={<PostDetail/>}/>
-          </Routes>
-        </div>
-      </>
-    </RecoilRoot>
+    <div className="App"></div>
   );
 }
 
