6fa03b7e398dcea6dea228bca071ef2b780a89c3
Uguls <chobozaex@gmail.com>
2024-05-23 16:14:13 +0900

    .

--- my_front/src/App.jsx
+++ my_front/src/App.jsx
@@ -1,13 +1,50 @@
 import "./App.css";
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
+import TodoList from "./components/TodoList";
+import {useRef, useState} from "react";
+
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createDate: new Date().getTime(),
+  }
+]
 
 function App() {
+  const idRef = useRef(3);
+  const [todo, setTodo] = useState([]);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
-      <div>TOdo List</div>
+      <TodoEditor onCreate={onCreate}/>
+      <TodoList />
     </div>
   );
 }
