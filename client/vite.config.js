693e438f82791efe9b8b126b2856ac033d03acf0
Uguls <chobozaex@gmail.com>
2024-09-11 21:29:55 +0900

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,11 @@
-import {defineConfig} from "vite";
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import {nodePolyfills} from "vite-plugin-node-polyfills";
 
-export default defineConfig(()=>{
-	return {
-		build: {
-			outDir: 'build'
-		},
-		plugins: [
-			react(),
-			nodePolyfills(),
-		],
-	}
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		nodePolyfills(),
+	],
+})
